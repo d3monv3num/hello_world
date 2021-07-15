e92038aa49ff723e454030f1c2b75e7693ab0fe0
Naman Anand <77112679+d3monv3num@users.noreply.github.com>
2021-07-15 10:22:35 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        header className = "App-header" >
+        <
+        img src = { logo }
+        className = "App-logo"
+        alt = "logo" / >
+        <
+        p >
+
+        Hello react < code > < /code>  < /
+        p > <
+        a className = "App-link"
+        href = "https://reactjs.org"
+        target = "_blank"
+        rel = "noopener noreferrer" >
+        Lets get started <
+        br / > <
+        button > Feeling Excited < /button>< /
+        a >
+        <
+        /
+        header > <
+        /div>
+    );
+}
+
+export default App;
